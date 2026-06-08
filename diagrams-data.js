332edb1f8473bc10222f3a1df10980ae7d71a8ee
bbtechrule <bbtechrule@gmail.com>
2026-06-08 18:19:44 +0530

    Update and rename diagrams.js to diagrams-data.js

---
+++ diagrams-data.js
@@ -0,0 +1,14 @@
+const diagrams = [
+  {
+    title: "Animal Cell",
+    image: "https://raw.githubusercontent.com/USERNAME/REPO/main/images/animal_cell.png"
+  },
+  {
+    title: "Plant Cell",
+    image: "https://raw.githubusercontent.com/USERNAME/REPO/main/images/plant_cell.png"
+  },
+  {
+    title: "DNA Structure",
+    image: "https://raw.githubusercontent.com/USERNAME/REPO/main/images/dna.png"
+  }
+];
