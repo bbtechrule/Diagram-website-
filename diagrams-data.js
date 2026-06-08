2c7526ce1a6679039d06baba603d2f2a8024ff7b
bbtechrule <bbtechrule@gmail.com>
2026-06-08 21:16:34 +0530

    Update diagrams-data.js

--- diagrams-data.js
+++ diagrams-data.js
@@ -1,14 +1,5 @@
-const diagrams = [
-  {
-    title: "Animal Cell",
-    image: "https://raw.githubusercontent.com/USERNAME/REPO/main/images/animal_cell.png"
-  },
-  {
-    title: "Plant Cell",
-    image: "https://raw.githubusercontent.com/USERNAME/REPO/main/images/plant_cell.png"
-  },
-  {
-    title: "DNA Structure",
-    image: "https://raw.githubusercontent.com/USERNAME/REPO/main/images/dna.png"
-  }
-];
+const diagrams = {
+  "animal cell": "https://raw.githubusercontent.com/bbtechrule/Diagram-website-/main/images/animal_cell.png",
+  "plant cell": "https://raw.githubusercontent.com/bbtechrule/Diagram-website-/main/images/plant_cell.png",
+  "dna structure": "https://raw.githubusercontent.com/bbtechrule/Diagram-website-/main/images/dna.png"
+};
